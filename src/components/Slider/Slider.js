df36247c55ca34c676f94ef3446ed796ba696b15
Sona Mathew <sonamathewc@gmail.com>
2022-11-20 19:04:35 +0530

    add slider

---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import './Slider.css'
+import Slider1 from '../../images/Slider1.jpg'
+import Slider2 from '../../images/Slider2.jpg'
+import Slider3 from '../../images/Slider3.jpg'
+import Slider4 from '../../images/Slider4.jpg'
+import Slider5 from '../../images/Slider5.jpg'
+
+
+
+
+const Slider = () => {
+  return (
+    <Carousel autoPlay="true" className='carousel'>
+                <div>
+                    <img src={Slider1}  />
+                    
+                </div>
+                <div>
+                    <img src={Slider2} />
+                  
+                </div>
+                <div>
+                    <img src={Slider3} height="600px" />
+                    
+                </div>
+                <div>
+                    <img src={Slider4} height="600px" />
+                    
+                </div>
+                <div>
+                    <img src={Slider5} height="600px"/>
+                  
+                </div>
+            </Carousel>
+  )
+}
+
+export default Slider
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import Header from '../components/Header/Header'
+import Slider from '../components/Slider/Slider'
 
 const Home = () => {
   return (
-    <div><Header/></div>
+    <div>
+    <>
+    <Header/>
+    <Slider/>
+    </>
+    </div>
   )
 }
 
