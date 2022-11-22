6004974ee948826c6d1202f6b89852a2ee925135
Sona Mathew <sonamathewc@gmail.com>
2022-11-22 19:13:00 +0530

    add login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import '../App.css'
+import Logo from "../images/logo.png"
+import {Link} from "react-router-dom"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='login'>
+    <div className="login-bg">
+    <img src={Logo} alt="" />
+    </div>
+    <div className="login-content">
+      <div className="form-bx">
+        <h2>Login</h2>
+        <form action="">
+          <div className="input-bx">
+              <span>Username</span>
+              <input type="text" name='username' />
+          </div>
+          <div className="input-bx">
+              <span>Password</span>
+              <input type="password" name='password' />
+          </div>
+          <div className="input-bx">
+            <button type='submit'>Login</button>
+          </div>
+          <div className="input-bx">
+            <p>Don't have an account?<span><Link to="/register">Register</Link></span></p> 
+          </div>
+        </form>
+      </div>
+    </div>
+    </div>
   )
 }
 
