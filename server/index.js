940c800731849c74d4ea80fad3313ab2c9e992dc
Sreyaea <sreya.ea@gmail.com>
2022-11-30 18:25:04 +0530

    login,register

--- server/index.js
+++ server/index.js
@@ -18,37 +18,45 @@ app.post("/register", (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
     
-    db.query("INSERT INTO User_details(user_id,username, email, password) SELECT COALESCE(MAX(user_id), 0)+1,'" + username + "','" 
-     + email + "','" + password + "' from User_details" ,(err, result) => {
-        if(err) {
-            console.log(err);
-        } else {
-            console.log("Successfull");
-            res.send("Values Inserted");
+    db.query("SELECT username from User_details where username='" + username + "'",(err,result) => {
+        if(result.length > 0){
+            return res.json({ status: "error", error: "User "+username+" is already registered"});
         }
-      }
-    );
-    
+        else{
+            db.query("INSERT INTO User_details(user_id,username, email, password) SELECT COALESCE(MAX(user_id), 0)+1,'" + username + "','" 
+            + email + "','" + password + "' from User_details" ,(err, result) => {
+            if(err) {
+                    console.log(err);
+                    return res.json({ status: "error", error: err});
+            } else {
+                    console.log("Successfull");
+                    //res.send("User "+username+" is successfully registered");
+                    return res.json({ status: "ok", error: ""});
+            }
+        }
+      );
+    }    
+    })
 });
 
 app.post('/login',(req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    db.query("SELECT * from User_details where username = '" + username+ 
-    "' AND password = '" + password + "'",
-    (err,result) => {
+    db.query("SELECT password from User_details where username = '"+username+"'",(err,result) => 
+    {
         console.log(result.length);
-        if(err)  {
-            res.send(err);
+        if(result.length >0) {
+            if(result[0].password==password)
+                return res.json({ status: "ok", error: ""});
+            else{
+                return res.json({ status: "error", error: "Invalid password"});
+            }
         }
-        else if(result.length >0) {
-            res.send("Successfully logged in");
+        else{
+            return res.json({ status: "error", error: "Not a registered user"});
         }
-        else  
-            res.send("Wrong username/or password combination!");
-        
     }
- )
+)
 });
 
 
