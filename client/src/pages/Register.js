aea51ddd563805db5fec1e199f69fad6fff71e9f
Sona Mathew <sonamathewc@gmail.com>
2022-11-30 21:28:12 +0530

    add register frontend

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../App.css'
 import Logo from "../images/logo.png"
 import {Link} from "react-router-dom"
-
+import axios from "axios"
 
 
 const Register = () => {
+  const [inputs,setInputs]=useState({
+    username:"",
+    email:"",
+    password:""
+  })
+
+  const handleChange = e=>{
+    setInputs(prev=>({...prev,[e.target.name]:e.target.value}))
+  }
+
+  const handleSubmit= async (e) =>{
+    e.preventDefault()
+    try{
+      const res = await axios.post("/register",inputs)
+      console.log(res);
+    }catch(err){
+console.log(err);
+    }
+  }
   return (
     <div className='login'>
     <div className="login-bg">
@@ -17,18 +36,18 @@ const Register = () => {
         <form action="">
         <div className="input-bx">
               <span>Username</span>
-              <input type="text" name='username' />
+              <input type="text" name='username' onChange={handleChange}/>
           </div>
           <div className="input-bx">
               <span>Email</span>
-              <input type="email" name='email' />
+              <input type="email" name='email' onChange={handleChange}/>
           </div>
           <div className="input-bx">
               <span>Password</span>
-              <input type="password" name='password' />
+              <input type="password" name='password' onChange={handleChange}/>
           </div>
           <div className="input-bx">
-            <button type='submit'>Register</button>
+            <button type='submit' onClick={handleSubmit}>Register</button>
           </div>
           <div className="input-bx">
             <p>Already have an account?<span><Link to="/login">Login</Link></span></p> 
