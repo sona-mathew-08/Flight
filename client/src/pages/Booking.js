5a6a7c98605ec7a2675801827dbc7e83af3d4efd
Sona Mathew <sonamathewc@gmail.com>
2022-12-05 18:11:58 +0530

    add fetch for booking and flight status

--- client/src/pages/Booking.js
+++ client/src/pages/Booking.js
@@ -1,34 +1,56 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import '../App.css'
-
+import axios from 'axios'
 
 const Booking = () => {
-    const flights=[{
+//code to display from backend
+    const [flightList, setflightList] = useState([]);
+
+    useEffect(() => {
+        
+        axios.get("/booking").then((response) => {
+            setflightList(response.data);
+          });
+      }, []);
+      
+
+/*
+    const flightList=[{
+        flightname:"AirIndia Express",
         source:"Istanbul",
         boarding:"10:30",
-        sCode:"IST",
+        boardingdate:"22/10/22",
+        //sCode:"IST",
         destination:"Ankara",
         arrival:"1:30",
-        dCode:"EST"
+        arrivaldate:"22/10/22",
+        //dCode:"EST"
     },
     {
+        flightname:"Indigo",
         source:"Istanbul",
         boarding:"10:30",
-        sCode:"IST",
+        boardingdate:"25/10/22",
+        //sCode:"IST",
         destination:"Ankara",
         arrival:"1:30",
-        dCode:"EST"
+        arrivaldate:"26/10/22",
+        //dCode:"EST"
     },
     {
+        flightname:"Indigo",
         source:"Istanbul",
         boarding:"10:30",
-        sCode:"IST",
+        boardingdate:"25/10/22",
+        //sCode:"IST",
         destination:"Ankara",
         arrival:"1:30",
-        dCode:"EST"
+        arrivaldate:"26/10/22",
+        //dCode:"EST"
     },
     ]
+   */  //above array to display the UI
   return (
     <div className="available-flight-section">
         <div className="tpd-plan">
@@ -37,13 +59,13 @@ const Booking = () => {
         
             
             {
-                flights.map((flight)=>(
+                flightList.map((flight)=>(
                     // console.log(flight.arrival);
                     <div className="crop depart">
                     <div className="context collapsed" >
                     
                     <div className="item it-1">
-                        <label className="trip-type depart">Departure</label>
+                        <label className="trip-type depart">{flight.flightname}</label>
                         <div className="route-dot">
                             <span className="point" ></span>
                             <span className="point" ></span>
@@ -52,13 +74,13 @@ const Booking = () => {
         
                         <div className="port-seg">
                             <div className="flight-seg origin">
-                                <div className="time">{flight.boarding}</div>
-                                <div className="port" style={{paddingLeft:"10px"}}>{flight.sCode}</div>
+                                <div className="time">{flight.boardingdate}</div>
+                                <div className="port" style={{paddingLeft:"10px"}}>{flight.boarding}</div>
                                 <div className="name" >{flight.source}</div>
                             </div>
                             <div className="flight-seg destination">
-                                <div className="time">{flight.arrival}</div>
-                                <div className="port" style={{paddingRight:"10px"}}>{flight.dCode}</div>
+                                <div className="time">{flight.arrivaldate}</div>
+                                <div className="port" style={{paddingRight:"10px"}}>{flight.arrival}</div>
                                 <div className="name">{flight.destination}</div>
                             </div>
                         </div>
--- client/src/pages/FlightStatus.js
+++ client/src/pages/FlightStatus.js
@@ -1,12 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {FaPlaneDeparture,FaPlaneArrival,FaPlane,FaCalendarCheck,FaClock,FaCity,FaRupeeSign}  from "react-icons/fa"
-import { Link } from 'react-router-dom'
+import React, {
+  useEffect,
+  useState
+} from 'react'
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  FaPlaneDeparture,
+  FaPlaneArrival,
+  FaPlane,
+  FaCalendarCheck,
+  FaClock,
+  FaCity,
+  FaRupeeSign
+} from "react-icons/fa"
+import {
+  Link
+} from 'react-router-dom'
 import './FlightStatus.css'
+import axios from 'axios'
 
 const FlightStatus = () => {
-  // const[flights,setFlights]=useState([])
-    const flights=[{
+  const [flightStatus, setFlightStatus] = useState([])
+  /*  const flightStatus=[{
         source:"Mumbai",
         types:"Departure Details",
         typed:"Arrival Details",
@@ -25,134 +39,359 @@ const FlightStatus = () => {
         lname:"Mathew",
         age:"20",
         travelclass:"Business",
-        phone:"123456"
+        contact:"123456"
     },
-    {
-        source:"Cochin",
-        types:"Departure Details",
-        typed:"Arrival Details",
-        boardingdate:"Fri Oct 27 2022",
-        arrivaldate:"Fri Oct 27 2022",
-        boardingairport:"Cochin Intnl Airport Limited",
-        arrivalairport:"John F. Kennedy",
-        boardingtime:"15:00:00",
-        destination:"New York",
-        arrivaltime:"23:00:00",
-        boardingcity:"Cochin,India",
-        arrivingcity:"New York,United States",
-        price:"20000",
-        passdetails:"Passenger Details",
-        fname:"Sona",
-        lname:"Mathew",
-        age:"20",
-        travelclass:"Business",
-        phone:"123456"
-    },
-
-
+    
     ]
+    */
+
+  useEffect(() => {
+
+    axios.get("/flight-status").then((response) => {
+      setFlightStatus(response.data);
+    });
+  }, []);
+  //ithil eth useEffect function aan work aakane enn nokkanam  
+  /*
+    useEffect(async()=>{
+       try{
+         const data= (await axios.get("/flight-status")).data
+         setFlightStatus(data)
+       }catch(err){
+         console.log(err);
+       }
+     },[])
+     */
+
+  return ( <
+    div className = "available-flight-section" >
+    <
+    div className = "tpd-plan" >
+    <
+    div className = "tp-flight-plan" >
+    <
+    div className = "container-fluid" >
 
 
-// useEffect(async()=>{
-//   try{
-//     const data= (await axios.get("/flights")).data
-//     setFlights(data)
-//   }catch(err){
-//     console.log(err);
-//   }
-// },[])
+    {
+      flightStatus.map((flight) => (
+        // console.log(flight.arrival);
+        <
+        div className = "crop depart" >
+        <
+        div className = "context collapsed" >
 
-  return (
-    <div className="available-flight-section">
-        <div className="tpd-plan">
-        <div className="tp-flight-plan">
-            <div className="container-fluid">
-        
-            
-            {
-                flights.map((flight)=>(
-                    // console.log(flight.arrival);
-                    <div className="crop depart">
-                    <div className="context collapsed" >
-                    
-                    <div className="item it-1">
-                        <label className="trip-type depart">Ticket Details</label>
-                             
-                        <div className="port-seg">
-                            <div className="flight-seg-origin">
-                              
-                              <div className="source" style={{paddingTop:"15px",paddingLeft:"25px",paddingRight:"25px"}}>
-                              <h2>{flight.source}</h2>  
-                              <FaPlane size="2rem" color='white' style={{paddingLeft:"285px"}}/>                                               
-                              </div>
-                                                            
-                              <div className="types" style={{paddingLeft:"25px",paddingBottom:"15px",paddingRight:"25px"}}>
-                                {flight.types}</div>                              
-                              <div className="boardingdate" style={{paddingTop:"10px",paddingLeft:"25px",paddingRight:"25px"}}>
-                                <FaCalendarCheck size="1rem" color='white'/>{' '}{flight.boardingdate}</div>                             
-                                                         
-                              <div className="boardingtime" style={{paddingLeft:"25px",paddingRight:"25px"}}>
-                                <FaClock size="1rem" color='white'/>{' '}{flight.boardingtime}</div> 
+        <
+        div className = "item it-1" >
+        <
+        label className = "trip-type depart" > Ticket Details < /label>
 
-                              <div className="boardingairpot" style={{paddingLeft:"25px",paddingRight:"25px"}}>
-                                <FaPlaneDeparture size="1rem" color='white'/>{' '}{flight.boardingairport}</div>   
+        <
+        div className = "port-seg" >
+        <
+        div className = "flight-seg-origin" >
 
-                              <div className="boardingcity" style={{paddingLeft:"25px",paddingRight:"25px"}}>
-                                <FaCity size="1rem" color='white'/>{' '}{flight.boardingcity}</div>        
+        <
+        div className = "source"
+        style = {
+          {
+            paddingTop: "15px",
+            paddingLeft: "25px",
+            paddingRight: "25px"
+          }
+        } >
+        <
+        h2 > {
+          flight.source
+        } < /h2>   <
+        FaPlane size = "2rem"
+        color = 'white'
+        style = {
+          {
+            paddingLeft: "285px"
+          }
+        }
+        />                                                <
+        /div>
 
-                              <div className="price" style={{paddingLeft:"25px",paddingTop:"50px",paddingRight:"25px"}}>Price=
-                              <FaRupeeSign size="0.7rem" color='white'/>{flight.price}</div>  
+        <
+        div className = "types"
+        style = {
+          {
+            paddingLeft: "25px",
+            paddingBottom: "15px",
+            paddingRight: "25px"
+          }
+        } > {
+          flight.types
+        } < /div>                               <
+        div className = "boardingdate"
+        style = {
+          {
+            paddingTop: "10px",
+            paddingLeft: "25px",
+            paddingRight: "25px"
+          }
+        } >
+        <
+        FaCalendarCheck size = "1rem"
+        color = 'white' / > {
+          ' '
+        } {
+          flight.boardingdate
+        } < /div>                             
 
-                              <hr style={{color:"white"}}/>
-                              <div className="passdetails" style={{paddingLeft:"25px",paddingBottom:"15px",paddingRight:"25px",paddingTop:"25px"}}>
-                                {flight.passdetails}</div> 
-                              <div className="name" style={{paddingLeft:"25px",paddingRight:"25px"}}>Name :{' '}{flight.fname}
-                                {' '}{flight.lname}</div> 
-                                <div className="age" style={{paddingLeft:"25px",paddingRight:"25px"}}>Age  :
-                                {' '}{flight.age}</div> 
-                                <div className="phone" style={{paddingLeft:"25px",paddingRight:"25px"}}>
-                                Phone:{' '}{flight.phone}</div> 
-                                <div className="travelclass" style={{paddingLeft:"25px",paddingRight:"25px"}}>
-                                Class:{' '}{flight.travelclass}</div> 
+        <
+        div className = "boardingtime"
+        style = {
+          {
+            paddingLeft: "25px",
+            paddingRight: "25px"
+          }
+        } >
+        <
+        FaClock size = "1rem"
+        color = 'white' / > {
+          ' '
+        } {
+          flight.boardingtime
+        } < /div> 
 
-                            </div>
+        <
+        div className = "boardingairpot"
+        style = {
+          {
+            paddingLeft: "25px",
+            paddingRight: "25px"
+          }
+        } >
+        <
+        FaPlaneDeparture size = "1rem"
+        color = 'white' / > {
+          ' '
+        } {
+          flight.boardingairport
+        } < /div>   
 
-                            <div className="flight-seg-destination">
-                            <div className="destination" style={{paddingTop:"15px",paddingLeft:"110px",paddingBottom:"2rem"}}>
-                                <h2>{flight.destination}</h2></div>
-                     
-                            <div className="typed" style={{paddingLeft:"110px",paddingBottom:"15px"}}>{flight.typed}</div>
+        <
+        div className = "boardingcity"
+        style = {
+          {
+            paddingLeft: "25px",
+            paddingRight: "25px"
+          }
+        } >
+        <
+        FaCity size = "1rem"
+        color = 'white' / > {
+          ' '
+        } {
+          flight.boardingcity
+        } < /div>        
 
-                            <div className="arrivaldate" style={{paddingLeft:"110px",paddingTop:"15px"}}>
-                                <FaCalendarCheck size="1rem" color='white'/>{' '}{flight.arrivaldate}</div>
+        <
+        div className = "price"
+        style = {
+          {
+            paddingLeft: "25px",
+            paddingTop: "50px",
+            paddingRight: "25px"
+          }
+        } > Price = <
+        FaRupeeSign size = "0.7rem"
+        color = 'white' / > {
+          flight.price
+        } < /div>  
 
-                            <div className="arrivaltime" style={{paddingLeft:"110px"}}>
-                                <FaClock size="1rem" color='white'/>{' '}{flight.arrivaltime}</div>
+        <
+        hr style = {
+          {
+            color: "white"
+          }
+        }
+        /> <
+        div className = "passdetails"
+        style = {
+          {
+            paddingLeft: "25px",
+            paddingBottom: "15px",
+            paddingRight: "25px",
+            paddingTop: "25px"
+          }
+        } > {
+          flight.passdetails
+        } < /div>  <
+        div className = "name"
+        style = {
+          {
+            paddingLeft: "25px",
+            paddingRight: "25px"
+          }
+        } > Name: {
+          ' '
+        } {
+          flight.fname
+        } {
+          ' '
+        } {
+          flight.lname
+        } < /div>  <
+        div className = "age"
+        style = {
+          {
+            paddingLeft: "25px",
+            paddingRight: "25px"
+          }
+        } > Age: {
+          ' '
+        } {
+          flight.age
+        } < /div>  <
+        div className = "phone"
+        style = {
+          {
+            paddingLeft: "25px",
+            paddingRight: "25px"
+          }
+        } >
+        Phone: {
+          ' '
+        } {
+          flight.contact
+        } < /div>  <
+        div className = "travelclass"
+        style = {
+          {
+            paddingLeft: "25px",
+            paddingRight: "25px"
+          }
+        } >
+        Class: {
+          ' '
+        } {
+          flight.travelclass
+        } < /div> 
 
-                            <div className="arrivalairport" style={{paddingLeft:"110px"}}>
-                                <FaPlaneArrival size="1rem" color='white'/>{' '}{flight.arrivalairport}</div>
-                           
-                            <div className="arrivingcity" style={{paddingLeft:"110px"}}>
-                                <FaCity size="1rem" color='white'/>{' '}{flight.arrivingcity}</div>
-                                <hr style={{color:"white",marginTop:"68px"}}/>
-                            </div>
-                        </div>
-                    </div>
-                   <div className='buttn'>
-                   <Link to="/Cancel-page">
-                    <button type='submit'><h3>Cancel</h3></button>
-                    </Link></div>
-                
-                    </div>
-                </div>
-                ))
-            }      
-                    
-                </div>
-            </div>
-        </div>
-</div>
-                                                               
+        <
+        /div>
+
+        <
+        div className = "flight-seg-destination" >
+        <
+        div className = "destination"
+        style = {
+          {
+            paddingTop: "15px",
+            paddingLeft: "110px",
+            paddingBottom: "2rem"
+          }
+        } >
+        <
+        h2 > {
+          flight.destination
+        } < /h2></div >
+
+        <
+        div className = "typed"
+        style = {
+          {
+            paddingLeft: "110px",
+            paddingBottom: "15px"
+          }
+        } > {
+          flight.typed
+        } < /div>
+
+        <
+        div className = "arrivaldate"
+        style = {
+          {
+            paddingLeft: "110px",
+            paddingTop: "15px"
+          }
+        } >
+        <
+        FaCalendarCheck size = "1rem"
+        color = 'white' / > {
+          ' '
+        } {
+          flight.arrivaldate
+        } < /div>
+
+        <
+        div className = "arrivaltime"
+        style = {
+          {
+            paddingLeft: "110px"
+          }
+        } >
+        <
+        FaClock size = "1rem"
+        color = 'white' / > {
+          ' '
+        } {
+          flight.arrivaltime
+        } < /div>
+
+        <
+        div className = "arrivalairport"
+        style = {
+          {
+            paddingLeft: "110px"
+          }
+        } >
+        <
+        FaPlaneArrival size = "1rem"
+        color = 'white' / > {
+          ' '
+        } {
+          flight.arrivalairport
+        } < /div>
+
+        <
+        div className = "arrivingcity"
+        style = {
+          {
+            paddingLeft: "110px"
+          }
+        } >
+        <
+        FaCity size = "1rem"
+        color = 'white' / > {
+          ' '
+        } {
+          flight.arrivingcity
+        } < /div> <
+        hr style = {
+          {
+            color: "white",
+            marginTop: "68px"
+          }
+        }
+        /> <
+        /div> <
+        /div> <
+        /div> <
+        div className = 'buttn' >
+        <
+        Link to = "/Cancel-page" >
+        <
+        button type = 'submit' > < h3 > Cancel < /h3></button >
+        <
+        /Link></div >
+
+        <
+        /div> <
+        /div>
+      ))
+    }
+
+    <
+    /div> <
+    /div> <
+    /div> <
+    /div>
+
   )
 }
 
